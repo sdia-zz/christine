67761690055581b9bb0c13641da882373ef1841f
Seydou Dia <sdia.pyc@gmail.com>
2017-01-08 15:57:43 +0100

    Create events.py

---
+++ events.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+
+
+class Event:
+    pass
+    
+    
+class TerroristAttack(Event):
+    pass
+    
+    
+class PlaneCrash(Event):
+    pass
+    
+    
+class Ww(Event):
+    pass
+    
+    
+    
