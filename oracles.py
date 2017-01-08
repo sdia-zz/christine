4df739f44678eacb6e7615e81a8c675ba9da74b6
Seydou Dia <sdia.pyc@gmail.com>
2017-01-08 15:54:54 +0100

    Create oracles.py

---
+++ oracles.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+
+
+class Oracle:
+    pass
+    
+    
+class Wikipedia(Oracle):
+  
+    def __init__(self):
+        pass
+        
+    def update(self):
+        pass
+        
+    def train(self):
+        pass
+        
+    def predict(self):
+        pass
+        
+        
+class Reuters(Oracle):
+    pass
+    
+    
+class Afp(Oracle):
+    pass
