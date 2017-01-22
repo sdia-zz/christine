71e85fb0f7db1a1b154715c20168f440d862a57f
sdia <seydou.dia@olx.com>
2017-01-22 22:48:50 +0100

    fix table creation from original

--- app/wikipedia.sql
+++ app/wikipedia.sql
@@ -5,7 +5,7 @@ CREATE schema christine;
 --------------------------------------------------------------------------------
 -- christine.wikipedia_raw
 --------------------------------------------------------------------------------
-
+DROP TABLE IF EXISTS christine.wikipedia_raw;
 CREATE TABLE christine.wikipedia_raw(
   inserted_at TIMESTAMP WITHOUT TIME ZONE DEFAULT (now() AT TIME ZONE 'utc'),
   event_date DATE,
@@ -19,7 +19,7 @@ CREATE INDEX ON christine.wikipedia_raw (event_date);
 --------------------------------------------------------------------------------
 -- christine.wikipedia_parsed
 --------------------------------------------------------------------------------
-
+DROP TABLE IF EXISTS christine.wikipedia_parsed;
 CREATE TABLE christine.wikipedia_parsed(
   inserted_at TIMESTAMP WITHOUT TIME ZONE DEFAULT (now() AT TIME ZONE 'utc'),
   event_date DATE,
@@ -34,4 +34,4 @@ CREATE INDEX ON christine.wikipedia_parsed (event_date);
 CREATE INDEX ON christine.wikipedia_parsed (event_type);
 
 DROP TABLE IF EXISTS christine.wikipedia_parsed_stg;
-CREATE TABLE christine.wikipedia_parsed_stg (LIKE christine.wikipedia_parsed);
+CREATE TABLE christine.wikipedia_parsed_stg (LIKE christine.wikipedia_parsed INCLUDING ALL);
